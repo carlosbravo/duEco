db89a3208744bd803b348405081bd681540a6054
Juliana <yulis.24a6@gmail.com>
2019-10-26 21:00:05 -0300

    Agregando icon duEco
    Commit previo a agregar conexion db

--- duEco/duEco.Android/MainActivity.cs
+++ duEco/duEco.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace duEco.Droid
 {
-    [Activity(Label = "duEco", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "duEco", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
