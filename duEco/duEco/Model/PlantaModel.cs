98fa203ff2c1589bc4d9e2c1cf679bb307f4b617
Juliana <yulis.24a6@gmail.com>
2019-10-27 20:48:05 -0300

    Listando Plantas desde SQLiteDB
    Se reemplaza lo que estaba hardcodeado

--- duEco/duEco/Model/PlantaModel.cs
+++ duEco/duEco/Model/PlantaModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace duEco.Model
@@ -30,5 +32,39 @@ namespace duEco.Model
             set { ImagenPortada = value; }
         }
 
+        #region SQLite
+        private static string _dbName;
+        private static string _path;
+        private static SQLiteConnection _db;
+
+
+        internal static List<PlantaModel> obtenerTodas()
+        {
+            // Acceso y conexion a la BD
+            _dbName = "duEcoSQLite.db3";
+            _path = Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal), _dbName);
+            _db = new SQLiteConnection(_path);
+
+            var query = _db.Table<Entidades.tbl_Planta>()
+                            .ToList();
+
+            List<PlantaModel> todasLasPlantas = new List<PlantaModel>();
+            if (query.Count > 0)
+            {
+                string urlImg = "~/Imagenes/iso_v7.PNG";
+                foreach (Entidades.tbl_Planta item in query)
+                {
+                    PlantaModel nPlanta = new PlantaModel();
+                    nPlanta.id = item.Pla_Id;
+                    nPlanta.nombre = item.Pla_Nombre;
+                    nPlanta.imagenPortada = urlImg;
+
+                    todasLasPlantas.Add(nPlanta);
+                }
+            }
+
+            return todasLasPlantas;
+        }
+        #endregion
     }
 }
--- duEco/duEco/Servicio/PlantaServicio.cs
+++ duEco/duEco/Servicio/PlantaServicio.cs
@@ -11,25 +11,26 @@ namespace duEco.Servicio
         internal static List<PlantaModel> todasLasPlantas()
         {
             /// reemplazar por llamada PlantaModel.todoPlanta() consultando BD
-            List<Model.PlantaModel> plantas = new List<Model.PlantaModel>();
+            //List<Model.PlantaModel> plantas = new List<Model.PlantaModel>();
 
-            Model.PlantaModel plantaTest = new Model.PlantaModel
-            {
-                id = "dfe1f51fe651fef1",
-                nombre = "Menta",
-                imagenPortada = ""
-            };
-            plantas.Add(plantaTest);
+            //Model.PlantaModel plantaTest = new Model.PlantaModel
+            //{
+            //    id = "dfe1f51fe651fef1",
+            //    nombre = "Menta",
+            //    imagenPortada = ""
+            //};
+            //plantas.Add(plantaTest);
 
-            Model.PlantaModel plantaTest2 = new Model.PlantaModel
-            {
-                id = "e8fe1f5frfe322ff",
-                nombre = "Tomate",
-                imagenPortada = ""
-            };
+            //Model.PlantaModel plantaTest2 = new Model.PlantaModel
+            //{
+            //    id = "e8fe1f5frfe322ff",
+            //    nombre = "Tomate",
+            //    imagenPortada = ""
+            //};
 
-            plantas.Add(plantaTest2);
-            return plantas;
+            //plantas.Add(plantaTest2);
+            //return plantas;
+            return PlantaModel.obtenerTodas();
         }
 
         internal static PlantaModel buscarPorId(string v)
